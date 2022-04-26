351b236b66bdff4cc100993f6b5ecc05bee554eb
Albert <abgev98@gmail.com>
2022-04-27 01:12:38 +0400

    changed store

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,20 @@ import {todosReducer, todos} from "../features/todos/todosSlice"
 const store = createStore(combineReducers({
     todos:todosReducer,
 }),{
-    todos:todos,
+    todos:{
+        mytodo:[
+            {
+                id:0,
+                name:"JS",
+                tasks:[{taskName:"45", description:"ok"}]
+            },
+            {
+                id:1,
+                name:"react",
+                tasks:[{taskName:"465", description:"ok"}]
+            }
+        ]
+    }
+    
 })
 export default store
